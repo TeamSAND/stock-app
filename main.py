50ccc20661f12ab72d2dc5702cdc6bce0b9d0481
Abhijit Mali <maliabhijit183@gmail.com>
2023-05-25 16:36:25 +0530

    Rest API Added for Sentiment

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 from flask import Flask, request, render_template
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.ensemble import RandomForestClassifier
-from nltk.corpus import stopwords
-import nltk
+import requests
 
-nltk.download('stopwords')
-
-set(stopwords.words('english'))
 app = Flask(__name__)
 
-#model = pickle.load(open('model2.pkl', 'rb'))
-#cv = pickle.load(open('transform.pkl', 'rb'))
-
-#df = pd.read_csv('Data.csv', encoding='ISO-8859-1')
-#rain = df[df['Date'] < '20150101']
-
-## Renaming column names for ease of access
-#list1= [i for i in range(25)]
-#new_Index=[str(i) for i in list1]
-#data.columns= new_Index
-
-#Implementing bag of words
-#train_data = cv.transform(headlines)
-
-## Implementing Random Forest Classifier
-#rfc = RandomForestClassifier(n_estimators=200, criterion='entropy')
-#rfc.fit(train_data, train['Label'])
+ENDPOINT_URL = "https://us-central1-aiplatform.googleapis.com/ui/projects/my-finsent-model/locations/us-central1/endpoints/205806586487111680:predict"
+HEADERS = {
+    "Authorization": "Bearer ya29.a0AWY7Ckkk406Q96EZRSz3De5G4hp2sUlbkM6I6g2AiYF-khtavZmCBUvcGKKrIX7mJ6aHY6rROeV16Uv3x-YGaJQqQyLwqh_z8Dxoe4J5bd3fDeljJM4DvwQ0OYrIHC590eNKGsp9O20_URF8PPKUN8Mmm4TtWMIurA0X_6waCgYKAcYSARASFQG1tDrpF11SKY_O70aj_54Ntv8xpg0174",
+    "Content-Type": "application/json"
+}
 
 @app.route('/')
 def my_form():
@@ -36,31 +15,31 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    stop_words = stopwords.words('english')
-    test_data = request.form['test_data'].lower()
+    test_data = request.form['test_data']
 
-    processed_data = ' '.join([word for word in test_data.split() if word not in stop_words])
+    data = {
+        "instances": {
+            "mimeType": "text/plain",
+            "content": test_data
+        }
+    }
 
-    sia = SentimentIntensityAnalyzer()
-    dd = sia.polarity_scores(text=processed_data)
-    compound = round((1 + dd['compound'])/2, 2)
-    
-    if compound > 0.5:
+    response = requests.post(ENDPOINT_URL, headers=HEADERS, json=data)
+    result = response.json()
+    print(result)
+    sentiment = result["predictions"][0]["sentiment"]
+    print(sentiment)
+    if sentiment is not None and sentiment >= 2:
+        compound1 = "2"
+        sentiment_label = "positive"
+    elif sentiment is not None and sentiment == 1:
         compound1 = "1"
-    elif compound < 0.5:
-        compound1 = "0"
+        sentiment_label = "Neutral"
     else:
         compound1 = "0"
+        sentiment_label = "negative"
 
-    # Categorize sentiment score
-    if compound > 0.5:
-        sentiment = "positive"
-    elif compound < 0.5:
-        sentiment = "negative"
-    else:
-        sentiment = "neutral"
-
-    return render_template('form.html', final=compound1, test_data=test_data, sentiment=sentiment)
+    return render_template('form.html', final=compound1, test_data=test_data, sentiment=sentiment_label)
 
 if __name__ == "__main__":
     app.run(debug=True)
