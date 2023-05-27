e7793927b5ab1b19f52582c51dd306d827297bc8
Abhijit Mali <maliabhijit183@gmail.com>
2023-05-28 00:13:22 +0530

    added fetch news api

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
 from flask import Flask, request, render_template
 import requests
+from flask import Flask, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 ENDPOINT_URL = "https://us-central1-aiplatform.googleapis.com/ui/projects/my-finsent-model/locations/us-central1/endpoints/205806586487111680:predict"
 HEADERS = {
-    "Authorization": "Bearer ya29.a0AWY7Ckkk406Q96EZRSz3De5G4hp2sUlbkM6I6g2AiYF-khtavZmCBUvcGKKrIX7mJ6aHY6rROeV16Uv3x-YGaJQqQyLwqh_z8Dxoe4J5bd3fDeljJM4DvwQ0OYrIHC590eNKGsp9O20_URF8PPKUN8Mmm4TtWMIurA0X_6waCgYKAcYSARASFQG1tDrpF11SKY_O70aj_54Ntv8xpg0174",
+    "Authorization": "Bearer ya29.a0AWY7CkkJLMPAHU3hIX1wXuMoBh6IISNDVJwnCV0Lyzs1OwQHXiXnN-_dKlpiEx4zEI98_sM723nUadTem_1OcfwmuYcmcB3etEaYLibtlqkI-xIN-Txs40PDhpfAsIo4ETekRwAmW_UteA233FjIjIxfTRuKxu4SO0WhaCgYKAb0SARASFQG1tDrptNC7FcXV1_DrMnl-E5z9YA0171",
     "Content-Type": "application/json"
 }
 
+cred = credentials.Certificate('finsentbysand-firebase.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def fetch_latest_news(company):
+    news_ref = db.collection(company).document('1')
+    news_doc = news_ref.get()
+    if news_doc.exists:
+        latest_news = news_doc.to_dict()
+        return latest_news
+    else:
+        return None
+    
 @app.route('/')
 def my_form():
     return render_template('form.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    test_data = request.form['test_data']
-
-    data = {
-        "instances": {
-            "mimeType": "text/plain",
-            "content": test_data
+    company = request.form['test_data']
+    # test_data = "stock gain 5 %"
+    latest_news = fetch_latest_news(company)
+    if latest_news:
+        test_data = latest_news.get('news')
+        data = {
+            "instances": {
+                "mimeType": "text/plain",
+                "content": test_data
+            }
         }
-    }
-
+    print(test_data)
     response = requests.post(ENDPOINT_URL, headers=HEADERS, json=data)
     result = response.json()
     print(result)
-    sentiment = result["predictions"][0]["sentiment"]
-    print(sentiment)
+    if 'predictions' in result:
+        sentiment = result["predictions"][0]["sentiment"]
+    else:
+        return " Auth Token Error "
+        # sentiment = 2 
+    # print(sentiment)
+    # sentiment = 0
     if sentiment is not None and sentiment >= 2:
         compound1 = "2"
         sentiment_label = "positive"
@@ -40,6 +64,11 @@ def my_form_post():
         sentiment_label = "negative"
 
     return render_template('form.html', final=compound1, test_data=test_data, sentiment=sentiment_label)
+
+
+# @app.route('/news/latest')
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
